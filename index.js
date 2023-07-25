bd302bdefd83f8204a4aa2174df401d93299f865
André Roberto <andreroberto52@gmail.com>
2023-07-24 21:44:32 -0300

    Alterações para alterar do site e mudanças no css

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ function changeLogo() {
     "div.css-1dbjc4n.r-dnmrzs.r-1vvnge1"
   )
 
-  console.log("******************************************************")
-  console.log(divContainer)
-
   if (divContainer) {
-    console.log("Positivo e operante")
-
+    console.log("engine started")
     while (divContainer.firstChild) {
       divContainer.removeChild(divContainer.firstChild)
     }
@@ -20,17 +16,23 @@ function changeLogo() {
       "class",
       "r-13v1u17 r-4qtqp9 r-yyyyoo r-16y2uox r-8kz0gk r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp mama aqui melon musk"
     )
+
     newImage.src = chrome.runtime.getURL("twitter_logo.png")
+
+    newImage.style.width = "100%"
+    newImage.style.height = "auto"
+    newImage.style.objectFit = "contain"
 
     divContainer.innerHTML = ""
 
     divContainer.setAttribute("class", "css-1dbjc4n r-dnmrzs r-1vvnge1")
-
-    console.log(newImage)
+    divContainer.style.paddingTop = "20px"
+    divContainer.style.paddingBottom = "50px"
+    // divContainer.style.paddingLeft = "30px"
+    divContainer.style.width = "40px"
+    divContainer.style.height = "40px"
 
     divContainer.appendChild(newImage)
-
-    console.log(divContainer)
   }
 }
 
